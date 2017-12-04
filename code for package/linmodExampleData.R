8faa8f2b8862871508820db72d289cb6931b6296
R-Ladies Tbilisi <tbilisi@rladies.org>
2017-12-04 18:26:13 +0200

    Packages, codes

---
+++ code for package/linmodExampleData.R
@@ -0,0 +1,14 @@
+#' Example data for the linmod function
+#'
+#' A dataset containing data for fitting a linear model. Variables included:
+#'
+#' \itemize{
+#'   \item x. An 100 x 10 predictor matrix
+#'   \item y. An outcome vector with 100 values
+#' }
+#'
+#' @docType data
+#' @name linmodExampleData
+#' @usage data(linmodExampleData)
+#' @format A list with two elements, x and y
+NULL
