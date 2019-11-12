9ae5a97a98fefd8bef6bb40f4e95a24c324085e3
Ildiko Czeller <czeildi@gmail.com>
2019-11-12 07:43:06 +0100

    init script

---
+++ 2019_11_13_sheets/sheets.R
@@ -0,0 +1,27 @@
+library(googledrive)
+library(googlesheets4)
+library(readxl)
+
+# motivating example: read complex xlsx
+
+#' - contains summary tables
+#' - multiple header rows
+#' - excel data format
+
+# loop through worksheets
+
+# read and write public sheets
+
+# loop through google sheets worksheets
+
+# read cell formatting
+
+#' color coded data
+#' formula
+#' data validaton
+
+# read private sheet
+
+# create new sheet and manage permissions
+
+# auth in non-interactive mode
