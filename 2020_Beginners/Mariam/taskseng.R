48344adf5bdc8f8caeeda5d038ec2b2fd88b9438
R-Ladies Tbilisi <tbilisi@rladies.org>
2020-02-23 11:38:57 +0200

    Add files via upload

---
+++ 2020_Beginners/Mariam/taskseng.R
@@ -0,0 +1,51 @@
+###############################
+## R-Ladies Tbilisi         ###
+###############################
+
+#########################
+## PRACTICAL SESSION 1 ##
+#########################
+
+# 1.1. First find out what your current working directory is
+getwd()
+
+# Now set a new working directory
+setwd("/Users/MariamAsatiani/Documents/R")
+
+# Using help() or ? functions figure out what the following functions do: paste(); abline(); c()
+
+# 1.2. Create eight objects named var0, var1, var2, var3, var4, var5, var6, var7
+# with the following content: 5, 2+2, 4*5, 240/60, 4^15, "this is", "my object", "5"
+
+# 1.3. What is the difference between var0 and var7? 
+#var0 is numeric var7 is a character
+
+# 1.4. Do a summing operation with var0 and var3.
+
+# 1.5. Paste var5 and var6 into one new object called "var56".
+
+# 1.6. Using the function c() create a vector with the following elements: 1,2,3,4. 
+#name it a
+
+# Using the function c() create a vector with the following elements: 1,1,1,1. 
+# name it b
+
+# Using the function length() find out what is the length of a.
+
+# Sum a and b vectors and create a new vector c
+
+# Using the function isTRUE() and the operator == find out if a and b vectors are equal
+
+# Create a new vector v. Should include "hi", "hello", "hola" 
+
+# Using the function class(), find out what the classes of vectors a and v are. 
+
+# 1.7 Create a matrix
+##M1 <- matrix(c(1,1,2,2,3,3), nrow = 2, ncol = 3)
+
+# 1.8. Install and load the following packages: foreign, readr, readstata13.
+
+# 1.9. Read about these packages - what are they for?
+
+# 1.10. Save your R file in your working directory.
+
