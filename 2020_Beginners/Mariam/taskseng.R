c112346359a816124bf2a6bdcb0bffa464b63110
R-Ladies Tbilisi <tbilisi@rladies.org>
2020-09-25 12:25:22 +0300

    Add files via upload

---
+++ 2020_Beginners/Mariam/taskseng.R
@@ -0,0 +1,61 @@
+###############################
+## R-Ladies Tbilisi         ###
+###############################
+
+#########################
+## PRACTICAL SESSION 1 ##
+#########################
+
+# 1.1. Gaige ra aris sheni samushao garemo
+getwd()
+
+# daayene axali samushao garemo
+setwd("/Users/MariamAsatiani/Documents/R")
+
+# help() an ? punqciebis gamoyenebit caikitxe shemdegi punqciebis shesaxeb: paste(); abline(); c()
+
+# 1.2. sheqmeni rva obieqti. daarqvi var0, var1, var2, var3, var4, var5, var6, var7
+# romlebic sheicaven: 5, 2+2, 4*5, 240/60, 4^15, "this is", "my object", "5"
+
+# 1.3. ra gansxvavebaa var0 and var7 shoris? 
+
+# 1.4. daajame var0 and var3.
+
+# 1.5. var5 da var6 -is gamoyenebit sheqmeni axali obieqti da daarqvi "var56". 
+#minishneba: gamoiyene paste() punqcia
+
+# 1.6. c() punqciis gamoyenebit sheqmeni veqtori elementebit: 1,2,3,4. 
+#veqtors daarqvi a
+
+# c() amoyenebit sheqmeni veqtori elementebit: 1,1,1,1. 
+# veqtors daarqvi b
+
+# length() punqciis gamoyenebit gaarqvie a-s sigrdze.
+
+# daajame a da b veqtorebi da sheqmeni axali veqtori c
+
+# isTRUE() punqciis da == operatoris gamoyenebit daadgine tu a udris b-s
+
+# sheqmeni axali veqtori v, romelic sheicavs "hi", "hello", "hola" 
+
+# class() punqciis gamoyenebit daadgine a da v veqtorebis klasebi
+
+# 1.7 sheqmeni matrica
+##M1 <- matrix(c(1,1,2,2,3,3), nrow = 2, ncol = 3)
+
+# 1.8. gadmocere da daayene shemdegi paketebi: foreign, readr, readstata13.
+
+# 1.9. caikitxe am paketebis shesaxeb
+
+# 1.10. sheinaxe sheni R faili
+
+# 1.11 pirveli 20 mteli ricxvis jami udris:
+20*(20+1)/2 
+
+#pirveli 100 mteli ricxvis jami udris
+100*(100+1)/2
+
+#ganazogade es gamosaxuleba n obieqtis daxmarebit 
+
+
+
